ee50fbcdf557aab0fffe04c40c624f86cd8f7b1a
Angad Dubey <angaddubey@microsoft.com>
2025-01-14 12:54:18 +0530

    DP Strategy Complete Example Code

--- src/strategy/GoogleMaps.java
+++ src/strategy/GoogleMaps.java
@@ -20,12 +20,12 @@ class GoogleMaps1 {
         }
 }
 
-// Above Code has voilation for SRP and OCP
-// As an improvement we can take out the logic and Impelment a class for each type
+// Above Code has violation for SRP and OCP
+// As an improvement we can take out the logic and Implement a class for each type
 class CarPathFinder{void findPath(){}}
 class BikePathFinder{void findPath(){}}
 class WalkPathFinder{void findPath(){}}
-class GoogleMaps{
+class GoogleMaps2{
     // Functionality of Finding Path
     void findPath(String source, String destination, Mode mode){
         if(mode == Mode.CAR){
@@ -43,4 +43,51 @@ class GoogleMaps{
     }
 }
 
+// Above has also SRP and OCP violations as well as DI Issues.
+// DI - Classes shouldn't rely on other classes, it should rely on Interface.
+// Below is the Strategy Design Pattern
 
+interface PathCalculator{
+    void findPath(String source, String destination);
+}
+
+class CarPathCalculator implements PathCalculator{
+    public void findPath(String source, String destination){
+        System.out.println("This is Car Path Calculator");
+    }
+}
+
+class BikePathCalculator implements PathCalculator{
+    public void findPath(String source, String destination){
+        System.out.println("This is Bike Path Calculator");
+    }
+}
+
+class WalkPathCalculator implements PathCalculator{
+    public void findPath(String source, String destination){
+        System.out.println("This is Walk Path Calculator");
+    }
+}
+
+// ??????? Need to check Registry DP to Return only SingleTons - Only one object creation for whole program.
+class PathCalculatorFactory{
+    CarPathCalculator cpc = new CarPathCalculator();
+    BikePathCalculator bpc = new BikePathCalculator();
+    WalkPathCalculator wpc = new WalkPathCalculator();
+
+    public PathCalculator getMode(Mode mode){
+        if(mode == Mode.CAR) return cpc;
+        else if(mode == Mode.BIKE) return bpc;
+        else if(mode == Mode.WALK) return wpc;
+        return null;
+    }
+}
+
+public  class GoogleMaps{
+    void findPath(String source, String destination, Mode mode){
+        PathCalculatorFactory pcf = new PathCalculatorFactory();
+        PathCalculator pc = pcf.getMode(mode);
+        assert pc != null;
+        pc.findPath(source, destination);
+    }
+}
